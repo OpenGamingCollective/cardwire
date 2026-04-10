786bc611881e947396ce14d8d4603c4450cd749c
Luytan <luytan@khora.me>
2026-04-10 13:11:30 +0200

    Fix(core-gpu): Fix default gpu discovery by replacing boot_vga check by a fb0 check

--- crates/cardwire-core/src/gpu/discover.rs
+++ crates/cardwire-core/src/gpu/discover.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use std::fs;
 use std::io;
-use std::path::Path;
 
 use crate::gpu::models::Gpu;
 use crate::iommu::Device;
@@ -11,7 +10,7 @@ pub fn read_gpu(pci_devices: &HashMap<String, Device>) -> io::Result<HashMap<usi
         .values()
         .filter(|device| {
             device.class.as_str() == "0x030000" || // VGA compatible controller
-            device.class.as_str() == "0x380000"})  // Display controller
+            device.class.as_str() == "0x038000"})  // Display controller
         .map(|device| build_gpu(device))
         .collect::<io::Result<Vec<_>>>()?;
 
@@ -29,22 +28,25 @@ pub fn read_gpu(pci_devices: &HashMap<String, Device>) -> io::Result<HashMap<usi
 }
 
 fn build_gpu(device: &Device) -> io::Result<Gpu> {
-    let boot_vga_path = Path::new("/sys/bus/pci/devices")
-        .join(&device.pci_address)
-        .join("boot_vga");
-    let is_default = fs::read_to_string(boot_vga_path)?.trim() == "1";
-
     Ok(Gpu {
         id: 0, // reassigned after sorting
         name: device.device_name.clone(),
         pci: device.pci_address.clone(),
         render: drm_node_path(&device.pci_address, "render")?,
         card: drm_node_path(&device.pci_address, "card")?,
-        default: is_default,
+        default: check_default(&device.pci_address)?,
     })
 }
 
 fn drm_node_path(pci_address: &str, node_kind: &str) -> io::Result<String> {
     let by_path = format!("/dev/dri/by-path/pci-{pci_address}-{node_kind}");
     Ok(fs::canonicalize(by_path)?.to_string_lossy().into_owned())
+}
+fn check_default(pci_address: &str) -> io::Result<bool> {
+    let fb0_path =format!("/sys/class/graphics/fb0");
+    match fs::canonicalize(fb0_path) {
+        Ok(content) => Ok(content.to_string_lossy().
+            contains(pci_address)),
+        Err(_) => Ok(false)
+    }
 }
