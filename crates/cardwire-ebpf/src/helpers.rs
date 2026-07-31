67d5c926d5ab0e924b0b286d5035ce4ec77e0149
luytan <luytan@khora.me>
2026-07-31 22:05:13 +0200

    feat(cardwire-ebpf): add verifications to try_file_open, add RingBufs and finis writing the dentry_blocked function

--- crates/cardwire-ebpf/src/helpers.rs
+++ crates/cardwire-ebpf/src/helpers.rs
@@ -1,7 +1,7 @@
 use aya_ebpf::{helpers::bpf_get_current_pid_tgid, programs::LsmContext};
 
 use crate::{
-    CardwiredSetting, DAEMON_INDEX, HYBRID, MODE_INDEX, SMART, maps::{CW_BLOCKED_INO, CW_DAEMON_PID, CW_EXP_BLK_INO, CW_MODE, CW_SETTINGS}, vmlinux::{dentry, inode}
+    CardwiredSetting, DAEMON_INDEX, HYBRID, INTEGRATED, MANUAL, MODE_INDEX, SMART, maps::{CW_BLOCKED_INO, CW_DAEMON_PID, CW_EXP_BLK_INO, CW_MODE, CW_SETTINGS}, vmlinux::{dentry, inode}
 };
 
 use crate::ReturnCode;
@@ -9,6 +9,7 @@ use crate::ReturnCode;
 use aya_log_ebpf::info;
 
 /// Verify if the dentry's inode is inside CW_BLOCKED_INO or not
+#[inline(always)]
 pub unsafe fn is_dentry_blocked(ctx: &LsmContext, d: *mut dentry) -> Result<i32, i32> {
     // Get a mutable ptr to the inode
     let inode_ptr: *mut inode = unsafe { (*d).d_inode };
@@ -19,44 +20,75 @@ pub unsafe fn is_dentry_blocked(ctx: &LsmContext, d: *mut dentry) -> Result<i32,
 
     let inode: u64 = unsafe { (*inode_ptr).i_ino };
 
-    // Check if the inode is in the blocked list
-    if unsafe { CW_BLOCKED_INO.get(inode).is_some() } {
-        info!(ctx, "inode blocked");
-        return ReturnCode::ENOENT;
+    let mut blocked: bool = false;
+
+    'inode_check: {
+        // Check if the inode is in the blocked list
+        if unsafe { CW_BLOCKED_INO.get(inode).is_some() } {
+            info!(ctx, "inode blocked");
+            blocked = true;
+            break 'inode_check;
+        }
+        // We didn't match any inode, try with nvidia inodes
+        if unsafe { is_nvidia_setting_enabled() && CW_EXP_BLK_INO.get(inode).is_some() } {
+            blocked = true;
+            break 'inode_check;
+        }
     }
-    // We didn't match any inode, try with nvidia inodes
-    if unsafe { is_nvidia_setting_enabled() && CW_EXP_BLK_INO.get(inode).is_some() } {
-        return ReturnCode::ENOENT;
+
+    'end: {
+        if !blocked {
+            // exit and return success
+            break 'end;
+        }
+
+        // Get the current mode used
+        let mode = match CW_MODE.get(MODE_INDEX) {
+            Some(mode) => mode,
+            // If we can't get the mode, just exit the block and return success
+            None => break 'end,
+        };
+        if *mode == INTEGRATED || *mode == MANUAL {
+            // if integrated/manual, just block
+            return ReturnCode::ENOENT;
+        }
+        if *mode == SMART {
+            // TODO: in a future PR
+        }
     }
 
     ReturnCode::SUCCESS
 }
 
 /// Verify if the proc is cardwired, returns None if the map fails
+#[inline(always)]
 pub fn is_cardwired() -> Option<bool> {
     let proc_pid = bpf_get_current_pid_tgid() as u32;
     match CW_DAEMON_PID.get(DAEMON_INDEX) {
-        Some(pid) => Some(proc_pid.eq(pid)),
+        Some(pid) => Some(proc_pid == *pid),
         None => None,
     }
 }
 
 /// Verify if the current device mode is hybrid, returns None if the map fails
+#[inline(always)]
 pub unsafe fn is_hybrid() -> Option<bool> {
     match CW_MODE.get(MODE_INDEX) {
-        Some(mode) => Some(mode.eq(&HYBRID)),
+        Some(mode) => Some(*mode == HYBRID),
         None => None,
     }
 }
 
 /// Verify if the current device mode is smart, returns None if the map fails
-pub unsafe fn is_smart() -> bool {
+#[inline(always)]
+pub unsafe fn is_smart() -> Option<bool> {
     match CW_MODE.get(MODE_INDEX) {
-        Some(mode) => mode.eq(&SMART),
-        None => false,
+        Some(mode) => Some(*mode == SMART),
+        None => None,
     }
 }
 
+#[inline(always)]
 pub unsafe fn is_nvidia_setting_enabled() -> bool {
     match unsafe { CW_SETTINGS.get(CardwiredSetting::EXP_NVIDIA) } {
         Some(setting) => *setting,
