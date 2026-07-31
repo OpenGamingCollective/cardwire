dca9da68ada60163c66547f6452a30f0b8163ca3
luytan <luytan@khora.me>
2026-07-31 21:16:51 +0200

    feat(cardwire-ebpf): implement return code struct, getter functions and all

--- crates/cardwire-ebpf/src/main.rs
+++ crates/cardwire-ebpf/src/main.rs
@@ -4,13 +4,11 @@
 use aya_ebpf::{
     helpers::{bpf_get_current_comm, bpf_get_current_pid_tgid}, macros::lsm, programs::LsmContext
 };
-use aya_log_ebpf::info;
+use aya_log_ebpf::{error, info, warn};
 
 use crate::{
-    helpers::is_device_blocked, vmlinux::{dentry, file, inode}
+    helpers::{is_dentry_blocked, is_hybrid}, vmlinux::{dentry, file, inode}
 };
-
-const ENOENT: i32 = -2;
 
 #[allow(
     clippy::all,
@@ -29,6 +27,30 @@ mod helpers;
 
 mod maps;
 
+struct ReturnCode {}
+impl ReturnCode {
+    // Succes means we didn't block the process
+    const SUCCESS: Result<i32, i32> = Ok(0);
+    // ENOENT means we blocked the process, it can't see the file
+    const ENOENT: Result<i32, i32> = Ok(-2);
+}
+
+// Used of CW_DAEMON_PID array
+const DAEMON_INDEX: u32 = 0;
+
+struct CardwiredSetting {}
+impl CardwiredSetting {
+    const EXP_NVIDIA: u8 = 0;
+}
+/*
+    Modes
+*/
+const MODE_INDEX: u32 = 0;
+const INTEGRATED: u8 = 0;
+const HYBRID: u8 = 1;
+const MANUAL: u8 = 2;
+const SMART: u8 = 3;
+
 #[lsm(hook = "file_open")]
 pub fn lsm_file_open(ctx: LsmContext) -> i32 {
     match unsafe { try_file_open(ctx) } {
@@ -38,17 +60,33 @@ pub fn lsm_file_open(ctx: LsmContext) -> i32 {
 }
 
 unsafe fn try_file_open(ctx: LsmContext) -> Result<i32, i32> {
+    // If the mode is hybrid, we exit
+    match unsafe { is_hybrid() } {
+        Some(res) => {
+            if res {
+                return ReturnCode::SUCCESS;
+            }
+        }
+        None => {
+            // This error happen if either the array is not available or the index 0 of the array is
+            // empty
+            error!(&ctx, "EBPF is_hybrid produced an error, exiting");
+            return ReturnCode::SUCCESS;
+        }
+    }
+
     let d: *mut dentry = unsafe {
         // arg.0 of file_open is a file
         let file_ptr: *const file = ctx.arg(0);
         (*file_ptr).__bindgen_anon_1.f_path.dentry
     };
 
+    // if no dentry, exit
     if d.is_null() {
-        return Ok(0);
+        return ReturnCode::SUCCESS;
     }
 
-    unsafe { is_device_blocked(&ctx, d) }
+    unsafe { is_dentry_blocked(&ctx, d) }
 }
 
 #[cfg(not(test))]
