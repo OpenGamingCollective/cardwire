659f37038ac93d23d562794aff695de071482152
luytan <luytan@khora.me>
2026-07-31 19:35:24 +0200

    feat(cardwire-ebpf): add lsm_open_file hook and start implementing helper functions

---
+++ crates/cardwire-ebpf/src/helpers.rs
@@ -0,0 +1,27 @@
+use aya_ebpf::programs::LsmContext;
+
+use crate::vmlinux::{dentry, inode};
+
+use crate::ENOENT;
+
+use aya_log_ebpf::info;
+
+pub unsafe fn is_device_blocked(ctx: &LsmContext, d: *mut dentry) -> Result<i32, i32> {
+    // Get a mutable ptr to the inode
+    let inode_ptr: *mut inode = unsafe { (*d).d_inode };
+
+    if inode_ptr.is_null() {
+        return Ok(0);
+    }
+
+    let inode: u64 = unsafe { (*inode_ptr).i_ino };
+
+    // Check if the inode is in the blocked list
+    let inode_blocked = 622 as u64;
+    if inode == inode_blocked {
+        info!(ctx, "inode blocked");
+        return Ok(ENOENT);
+    }
+
+    Ok(0)
+}
--- crates/cardwire-ebpf/src/main.rs
+++ crates/cardwire-ebpf/src/main.rs
@@ -1,23 +1,52 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_ebpf::{
+    helpers::{bpf_get_current_comm, bpf_get_current_pid_tgid}, macros::lsm, programs::LsmContext
+};
 use aya_log_ebpf::info;
 
-#[xdp] // (4)
-pub fn xdp_hello(ctx: XdpContext) -> u32 {
-    // (5)
-    match unsafe { try_xdp_hello(ctx) } {
+use crate::{
+    helpers::is_device_blocked, vmlinux::{dentry, file, inode}
+};
+
+const ENOENT: i32 = -2;
+
+#[allow(
+    clippy::all,
+    dead_code,
+    improper_ctypes_definitions,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    unnecessary_transmutes,
+    unsafe_op_in_unsafe_fn,
+)]
+#[rustfmt::skip]
+mod vmlinux;
+
+mod helpers;
+
+#[lsm(hook = "file_open")]
+pub fn lsm_file_open(ctx: LsmContext) -> i32 {
+    match unsafe { try_file_open(ctx) } {
         Ok(ret) => ret,
-        Err(_) => xdp_action::XDP_ABORTED,
+        Err(ret) => ret,
     }
 }
 
-unsafe fn try_xdp_hello(ctx: XdpContext) -> Result<u32, u32> {
-    // (6)
-    info!(&ctx, "received a packet");
-    // (7)
-    Ok(xdp_action::XDP_PASS)
+unsafe fn try_file_open(ctx: LsmContext) -> Result<i32, i32> {
+    let d: *mut dentry = unsafe {
+        // arg.0 of file_open is a file
+        let file_ptr: *const file = ctx.arg(0);
+        (*file_ptr).__bindgen_anon_1.f_path.dentry
+    };
+
+    if d.is_null() {
+        return Ok(0);
+    }
+
+    unsafe { is_device_blocked(&ctx, d) }
 }
 
 #[cfg(not(test))]
