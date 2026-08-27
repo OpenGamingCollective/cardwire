2c9f83927e7e0657c8076dbee3e5bbd560d44fb7
luytan <luytan@khora.me>
2026-08-27 16:54:54 +0200

    feat(cardwire-gui): add help description for --background

--- crates/cardwire-gui/src/args.rs
+++ crates/cardwire-gui/src/args.rs
@@ -3,6 +3,6 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 pub struct CardwireArgs {
-    #[arg(short, long, num_args = 0..=1, default_missing_value = "true")]
+    #[arg(short, long, num_args = 0..=1, default_missing_value = "true", help("starts the GUI in background"))]
     pub background: Option<bool>,
 }
