e55b9056a8ce599eb0a338dd09a7da02553a7d32
luytan <luytan@khora.me>
2026-09-15 21:29:17 +0200

    fix(cardwired): use gpu type for systemtype check, and todo

--- crates/cardwire-cli/src/types.rs
+++ crates/cardwire-cli/src/types.rs
@@ -10,29 +10,30 @@ pub enum SystemType {
 }
 impl SystemType {
     pub fn from_gpulist(gpu_list: &BTreeMap<usize, GpuDevice>) -> Self {
-        let available_gpus: Vec<(usize, bool, bool)> = gpu_list
+        let available_gpus: Vec<(usize, bool, GpuType)> = gpu_list
             .iter()
             .filter(|(_, gpu)| gpu.device_type != GpuType::Unavailable)
-            .map(|(id, gpu)| (*id, gpu.default, gpu.device_type == GpuType::Discrete))
+            .map(|(id, gpu)| (*id, gpu.default, gpu.device_type.clone()))
             .collect();
 
         if available_gpus.len() != 2 {
             Self::Manual
         } else if available_gpus
             .iter()
-            .any(|(_, default, discrete)| *default && *discrete)
+            .any(|(_, default, device_type)| *default && *device_type == GpuType::Discrete)
             && available_gpus
                 .iter()
-                .any(|(_, default, discrete)| !*discrete && !*default)
+                .any(|(_, default, device_type)| *device_type != GpuType::Discrete && !*default)
         {
-            // Has a default discrete GPU and a non-default non-discrete GPU
+            // Has a default discrete GPU and a non-default non-discrete GPU, desktop and manual are
+            // pretty much the same, TODO
             Self::Desktop
         } else if available_gpus
             .iter()
-            .any(|(_, default, discrete)| *discrete && !*default)
+            .any(|(_, default, device_type)| *device_type == GpuType::Discrete && !*default)
             && available_gpus
                 .iter()
-                .any(|(_, default, discrete)| !*discrete && *default)
+                .any(|(_, default, device_type)| *device_type != GpuType::Discrete && *default)
         {
             // Has a non-default discrete GPU and a default non-discrete GPU
             Self::Laptop
