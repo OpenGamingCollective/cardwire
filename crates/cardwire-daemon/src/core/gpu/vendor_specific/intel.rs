53745fff849665395f997adc8d9820b0fcbcc3ce
luytan <luytan@khora.me>
2026-09-15 21:40:14 +0200

    docs(cardwired): add todo for xe ioctl in the future, if intel fails

--- crates/cardwire-daemon/src/core/gpu/vendor_specific/intel.rs
+++ crates/cardwire-daemon/src/core/gpu/vendor_specific/intel.rs
@@ -1,9 +1,9 @@
 use crate::core::gpu::models::GpuType;
 
 /// Get the gpu type for an intel GPU
-#[allow(unused, dead_code)]
 pub fn intel_get_device_type(pci_id: &str) -> GpuType {
     // PCI id reserved for iGPUs
+    // TODO: try with xe ioctl
     if pci_id == "0000:00:02.0" {
         GpuType::Integrated
     } else {
