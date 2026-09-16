a01b5c8eae6f1edf448b2f8c8b12a52a6d448d16
luytan <luytan@khora.me>
2026-09-16 20:32:48 +0200

    fix(cardwired): prevent un-available gpu inodes from getting searched

--- crates/cardwire-daemon/src/core/gpu/mod.rs
+++ crates/cardwire-daemon/src/core/gpu/mod.rs
@@ -7,5 +7,5 @@ pub use enumerator::GpuEnumerator;
 pub use generic::default_gpu::check_default_drm_class;
 #[expect(unused_imports)]
 pub use generic::display::{external_display_connected, is_gpu_active};
-pub use models::{DbusGpuDevice, GpuDevice, GpuVendor, PowerState};
+pub use models::{DbusGpuDevice, GpuDevice, GpuType, GpuVendor, PowerState};
 pub use vendor_specific::nvidia::{start_nvidia_powerd, stop_nvidia_powerd};
