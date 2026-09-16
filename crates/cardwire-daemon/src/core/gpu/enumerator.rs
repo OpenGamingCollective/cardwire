7893ceba03aca0bd7d2697113c7c9f814a053fed
luytan <luytan@khora.me>
2026-09-16 20:34:36 +0200

    refactor(cardwired): replace match with if let in vulkan drm nodes

--- crates/cardwire-daemon/src/core/gpu/enumerator.rs
+++ crates/cardwire-daemon/src/core/gpu/enumerator.rs
@@ -88,27 +88,23 @@ impl GpuEnumerator {
             let gpu_type = self.vulkan.get_gpu_type(pci_id);
             let gpu_name = self.vulkan.get_gpu_name(pci_id);
 
-            match (
+            if let (Some(card), Some(render)) = (
                 self.vulkan.get_gpu_card(pci_id),
                 self.vulkan.get_gpu_render(pci_id),
             ) {
-                (Some(card), Some(render)) => {
-                    let gpu_device = GpuDevice::new(
-                        gpu_name,
-                        device.clone(),
-                        render as u32,
-                        card as u32,
-                        None,
-                        gpu_vendor,
-                        None,
-                        gpu_type,
-                    );
-                    info!("{}: Used Vulkan to build", gpu_device.name());
-                    debug!("{:?}", gpu_device);
-                    return Ok(gpu_device);
-                }
-                // Fallback to vendor match
-                _ => {}
+                let gpu_device = GpuDevice::new(
+                    gpu_name,
+                    device.clone(),
+                    render as u32,
+                    card as u32,
+                    None,
+                    gpu_vendor,
+                    None,
+                    gpu_type,
+                );
+                info!("{}: Used Vulkan to build", gpu_device.name());
+                debug!("{:?}", gpu_device);
+                return Ok(gpu_device);
             }
         }
 
